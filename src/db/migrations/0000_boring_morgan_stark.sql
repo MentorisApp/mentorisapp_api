549a0fbae763cebb8512a802b84e2b12304d2b54
Luka Gurdulic <gurdulicluka@gmail.com>
2025-08-16 11:45:06 +0200

    migrations test files delete

--- src/db/migrations/0000_boring_morgan_stark.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "role_permissions" (
-	"role_id" integer NOT NULL,
-	"permission_id" integer NOT NULL,
-	CONSTRAINT "role_permissions_role_id_permission_id_pk" PRIMARY KEY("role_id","permission_id")
-);
---> statement-breakpoint
-ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_permission_id_permissions_id_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permissions"("id") ON DELETE cascade ON UPDATE no action;
