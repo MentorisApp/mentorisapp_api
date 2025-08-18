fcf543f6637caf957de4800fe557545f7096d709
Luka Gurdulic <gurdulicluka@gmail.com>
2025-08-18 22:16:19 +0200

    database generate and push in dev

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,18 @@
-// import { env } from "@env";
+import * as dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
+dotenv.config({ path: ".env.development" })
 
+const DATABASE_URL = process.env.DATABASE_URL
 
 export default defineConfig({
 	schema: "./src/db/schema/**/*.schema.ts",
 	out: "./src/db/migrations",
 	dialect: "postgresql",
-	// dbCredentials: {
-	// 	url: url,
-	// },
-});
+	dbCredentials: {
+		url: DATABASE_URL!,
+	},
+	verbose: true,
+})
+
 
